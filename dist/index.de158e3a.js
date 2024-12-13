e780c1a4bce66e3d20ae3869e8ec25cf2730b441
Mstyslav Soroka <mstyslav2010@gmail.com>
2024-12-13 19:15:08 +0000

    first commit

---
+++ dist/index.de158e3a.js
@@ -0,0 +1,40 @@
+const form = document.getElementById('form');
+const table = document.getElementById('table_content');
+let students = [];
+function show() {
+    table.innerHTML = '';
+    students.forEach((student, i)=>{
+        const row = document.createElement('tr');
+        row.innerHTML = `
+      <td>${student.name}</td>
+      <td>${student.surname}</td>
+      <td>${student.age}</td>
+      <td>${student.course}</td>
+      <td>${student.faculty}</td>
+      <td>
+        <button onclick="deleteStudent(${i})">Delete</button>
+      </td>
+    `;
+        table.appendChild(row);
+    });
+}
+form.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    const newStudent = {
+        name: document.getElementById('name').value,
+        surname: document.getElementById('surname').value,
+        age: document.getElementById('age').value,
+        course: document.getElementById('course').value,
+        faculty: document.getElementById('faculty').value
+    };
+    students.push(newStudent);
+    show();
+    console.log(JSON.stringify(students));
+    form.reset();
+});
+function deleteStudent(index) {
+    students.splice(index, 1);
+    show();
+}
+
+//# sourceMappingURL=index.de158e3a.js.map
